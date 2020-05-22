d86a47547cf13680808e20f6d7e557195a1726e7
Mika Jones <mlljones@icloud.com>
2020-05-22 18:28:38 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MS_Teams_API;
+
+namespace MS_Teams_CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MS_Teams_API.Core CLI = new Core(args);
+            CLI.Run();
+            Console.WriteLine("You can close this window.");
+        }
+    }
+}
